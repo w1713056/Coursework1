3b64c2b9579e551dea0e29c9eb57aaaab8a7fe64
w1713056 <khalish.zeeshan@live.co.uk>
2019-11-11 23:20:23 +0000

    This is the third floor
    this is all the items for the third floor - ZEESHAN

---
+++ Assets/Zeeshan/Scripts/Key.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        Key_Display Key_Display = GameObject.Find("Canvas").GetComponent<Key_Display>();
+        Player player = other.GetComponent<Player>();
+
+        if (other.tag == "Player")
+        {
+            if (player != null)
+            {
+                player.HasKey = true;
+                Key_Display.DontDisplayKey();
+                Key_Display.counter++;
+                Key_Display.KeyUpdate();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Zeeshan/Scripts/Key_Display.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Key_Display : MonoBehaviour
+{
+
+    public Text key_text;
+    [SerializeField]
+    public int counter = 0;
+    [SerializeField]
+    private GameObject KeyDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        key_text.text = "KEYS : " + counter;
+    }
+
+    public void KeyUpdate()
+    {
+        key_text.text = "KEYS : " + counter;
+    }
+
+    public void DontDisplayKey()
+    {
+
+        KeyDisplay.SetActive(false);
+    }
+}
